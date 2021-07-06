78680b5bf7e0ab3ca2cbc26d0f63c540991b93f5
chigovv <chigovv@gmail.com>
2021-07-06 14:50:37 +0300

    new Thread

--- app/src/main/java/techmarket/uno/trafficlight01/MainActivity.java
+++ app/src/main/java/techmarket/uno/trafficlight01/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     private LinearLayout bulb1;
     private LinearLayout bulb2;
     private LinearLayout bulb3;
-
+    private boolean start_stop = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,10 +18,35 @@ public class MainActivity extends AppCompatActivity {
         bulb1 = findViewById(R.id.bulb1);
         bulb2 = findViewById(R.id.bulb2);
         bulb3 = findViewById(R.id.bulb3);
+        //start_stop = true;
     }
 
     public void onClickStart(View view) {
+        new Thread(new Runnable() { //важно очень!!!!!!!!!!!!
+
+            @Override //данная ветка будет работать независимо от основной ветки
+            public void run()
+            {
+                while (start_stop)
+                {
+                    try
+                    {
+                        Thread.sleep(3000);
+                    }
+                    catch (InterruptedException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
 
 
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        start_stop = false;
     }
 }
