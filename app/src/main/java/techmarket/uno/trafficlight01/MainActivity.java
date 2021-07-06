66956b44395bf27c514e9b66e71e1217a0289848
chigovv <chigovv@gmail.com>
2021-07-06 13:28:48 +0300

    MainActivity changed

--- app/src/main/java/techmarket/uno/trafficlight01/MainActivity.java
+++ app/src/main/java/techmarket/uno/trafficlight01/MainActivity.java
@@ -4,15 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private LinearLayout bulb1;
+    private LinearLayout bulb2;
+    private LinearLayout bulb3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bulb1 = findViewById(R.id.bulb1);
+        bulb2 = findViewById(R.id.bulb2);
+        bulb3 = findViewById(R.id.bulb3);
     }
 
     public void onClickStart(View view) {
+
+
     }
 }
